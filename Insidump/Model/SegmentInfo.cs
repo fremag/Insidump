d9596e0f4105cbec64a623ddbe851b0583b291b6
fremag <fremag.gh@gmail.com>
2025-07-05 15:41:52 +0200

    v 0.4.1 Segments

--- Insidump/Model/SegmentInfo.cs
+++ Insidump/Model/SegmentInfo.cs
@@ -1,24 +1,30 @@
 using Insidump.Core.ObjectTable;
 using Microsoft.Diagnostics.Runtime.Interfaces;
+using Terminal.Gui.ViewBase;
 
 namespace Insidump.Model;
 
-public class SegmentInfo(IClrSegment segment)
+public class SegmentInfo(int n, IClrSegment segment)
 {
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public ulong Address { get; } = segment.Address;
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public ulong End { get; } = segment.End;
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public ulong FirstObjectAddress { get; } = segment.FirstObjectAddress;
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public bool IsPinned { get; } = segment.IsPinned;
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public string Kind { get; } = segment.Kind.ToString();   
-    [TableColumn(Format = "{0}", Sortable = true)]
-    public string Flags { get; } = segment.Flags.ToString();
-    [TableColumn(Format = "{0}", Sortable = true)]
+    
+    [TableColumn(Format = "#{0}", Sortable = true, Alignment = Alignment.End)]
+    public int N { get; } = n;
+
+    [TableColumn(Format = "{0:###,###,###,##0}", Sortable = true, Alignment = Alignment.End)]
     public ulong Length { get; } = segment.Length;
-    [TableColumn(Format = "{0}", Sortable = true)]
+    
+    [TableColumn(Format = "{0:x}", Sortable = true, Alignment = Alignment.End)]
     public ulong Start { get; } = segment.Start;
+    [TableColumn(Format = "{0:x}", Sortable = true, Alignment = Alignment.End)]
+    public ulong End { get; } = segment.End;
+    [TableColumn(Format = "{0}", Sortable = true, Alignment = Alignment.End)]
+    public bool IsPinned { get; } = segment.IsPinned;
+    [TableColumn(Format = "{0}", Sortable = true, Alignment = Alignment.End)]
+    public string Kind { get; } = segment.Kind.ToString();   
+    [TableColumn(Format = "{0}", Sortable = true, Alignment = Alignment.End)]
+    public string Flags { get; } = segment.Flags.ToString();
+    [TableColumn(Format = "{0:x}", Sortable = true, Alignment = Alignment.End)]
+    public ulong Address { get; } = segment.Address;
+    [TableColumn(Format = "{0:x}", Sortable = true, Alignment = Alignment.End)]
+    public ulong FirstObjectAddress { get; } = segment.FirstObjectAddress;
 }
